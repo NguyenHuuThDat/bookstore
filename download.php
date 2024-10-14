bf0b5a9f21c9d0c2061612a3f8bc234e306c0f97
Nguyễn Hữu Thành Đạt <111550539+NguyenHuuThDat@users.noreply.github.com>
2024-10-14 15:14:45 +0700

    Update SMTP account

--- download.php
+++ download.php
@@ -41,8 +41,8 @@
         $mail->isSMTP();                                        //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                   //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                               //Enable SMTP authentication
-        $mail->Username   = '';                                 //SMTP username
-        $mail->Password   = '';                                 //SMTP password
+        $mail->Username   = 'huudat@breatic.tech';                                 //SMTP username
+        $mail->Password   = 'nlvz oeqe qldo xoup';                                 //SMTP password
         // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;     //Enable implicit TLS encryption
         $mail->Port       = 587;                                //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
